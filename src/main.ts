54051ab4291a0f9cf940ecb6c296ea075398f74b
Adan-Godoy <130314516+Adan-Godoy@users.noreply.github.com>
2025-05-13 01:12:32 -0400

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para cualquier origen
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
